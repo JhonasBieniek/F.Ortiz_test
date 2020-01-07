aca8f30d4c2d7a5c65c2145c5bad3d2e365b6535
Marcos <berssa.net@gmail.com>
2020-01-07 11:08:27 -0300

    ajuste para um layout

--- src/app/pedido/orc-listar/orc-listar.component.ts
+++ src/app/pedido/orc-listar/orc-listar.component.ts
@@ -1,17 +1,62 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { ClientService } from '../../shared/services/client.service.component';
+import { MatDialog } from '@angular/material';
+import { Router} from '@angular/router';
+import page from './steps.json';
 
 @Component({
   selector: 'app-orc-listar',
-  templateUrl: './orc-listar.component.html',
+  templateUrl: '../default.html',
   styleUrls: ['./orc-listar.component.css']
 })
+
 export class OrcListarComponent implements OnInit {
 
-  rota:string = "orcamentos"
+  editing = {};
+  rows:any = [];
+  temp:any = [];
+  selected:any = [];
+  page:any = page;
+  steps: any = this.page.pedidos;
+  defaultTab = 0;
 
-  constructor() { }
+  itemSelected
 
+  loadingIndicator = true;
+  reorderable = true;
+
+  isEditable = {};               
+  
+  constructor(
+    private clientservice: ClientService,
+    private router: Router,
+    private dialog: MatDialog) {
+      this.loadData()      
+  }
+  
+  loadData(){
+    this.clientservice.getOrcamentos().subscribe((res:any) =>{
+      let i = 0;
+      this.steps.forEach(e => {
+        this.temp[i] = res.data.filter(d => d.status == e.step);
+        i++;
+      });
+      this.rows = [...this.temp];
+    });                     
+  }
   ngOnInit() {
+  }
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+    this.rows[this.defaultTab] = this.temp[this.defaultTab].filter(function(d) {
+      if( d.cliente.nome_fantasia.toLowerCase().indexOf(val) !== -1 || !val ||
+          d.representada.nome_fantasia.toLowerCase().indexOf(val) !== -1 || !val ||
+          d.num_pedido.toLowerCase().indexOf(val) !== -1 || !val )
+      return d
+    }); 
+  }
+  add(){
+    this.router.navigate(['/pedido/orcamento']);
   }
 
 }
