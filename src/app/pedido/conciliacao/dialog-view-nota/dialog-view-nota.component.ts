7fa80a066e960f0b6760966f405f837a5b343fe2
Marcos <berssa.net@gmail.com>
2020-02-29 19:46:28 -0300

    View concilicao nota

--- src/app/pedido/conciliacao/dialog-view-nota/dialog-view-nota.component.ts
+++ src/app/pedido/conciliacao/dialog-view-nota/dialog-view-nota.component.ts
@@ -9,19 +9,48 @@ import { ClientService } from '../../../shared/services/client.service.component
 })
 export class DialogViewNotaComponent implements OnInit {
   dados:any ;
+  rows:any = [];
+  temp:any = [];
+  selected:any = [];
+  
+  steps: any = [
+    {
+      titulo: "Produtos do Pedido",
+      step: true,
+      index: 0
+    }
+  ];
+
+  defaultTab = 0;
+
   constructor(
     private clientservice: ClientService,
     public dialogRef: MatDialogRef<DialogViewNotaComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-  ) { }
+  ) {
+    this.loadData();
+   }
   
   loadData(){
     this.clientservice.getNotasID(this.data.id).subscribe((res:any)=> {
       this.dados = res.data;
+      let i = 0;
+      this.temp[i] = res.data.pedido.pedido_produtos;
+      this.rows = [...this.temp];
     })
+  }
+
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+    this.rows[this.defaultTab] = this.temp[this.defaultTab].filter(d => {
+      if( d.produto.codigo.toLowerCase().indexOf(val) !== -1 || !val 
+      || d.produto.nome.toLowerCase().indexOf(val) !== -1 || !val)
+      return d
+    });
   }
 
   ngOnInit() {
   }
+
 
 }
