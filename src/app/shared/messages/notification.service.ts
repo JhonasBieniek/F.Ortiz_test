9a81d849954b7a4d1c1f9becd947cbbf10d00cec
Lealx13 <afonso_martiusi@hotmail.com>
2021-10-14 16:34:50 -0300

    adjust: Change emmiter to snackbar

--- src/app/shared/messages/notification.service.ts
+++ src/app/shared/messages/notification.service.ts
@@ -1,9 +1,19 @@
 import { EventEmitter } from "@angular/core";
+import { MatSnackBar } from "@angular/material";
 
 export class NotificationService{
     notifier = new EventEmitter<string>()
 
-    notify(message: string){
+    constructor(private snackBar: MatSnackBar){}
+
+    
+    notify_Old(message: string){
         this.notifier.emit(message)
+    }
+    
+    notify(message: string){
+        this.snackBar.open(message, undefined, {
+            duration: 4000,
+        });
     }
 }
